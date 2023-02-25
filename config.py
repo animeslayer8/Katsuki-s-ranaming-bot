f76ff17e4ee65af2b691309c903cc697525722c5
animeslayer8 <126319219+animeslayer8@users.noreply.github.com>
2023-02-25 16:12:40 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@ import re, os
 
 id_pattern = re.compile(r'^.\d+$') 
 
-API_ID = os.environ.get("API_ID", "")
+API_ID = os.environ.get("7165358", "")
 
-API_HASH = os.environ.get("API_HASH", "")
+API_HASH = os.environ.get("304228f1dc7b9c86ef1c8d83cdc5da85", "")
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
+BOT_TOKEN = os.environ.get("6144893657:AAFtyY6jWx_-m7dYwNZgrAPfFmZhZIsVZ7I", "") 
 
-FORCE_SUB = os.environ.get("FORCE_SUB", "") 
+FORCE_SUB = os.environ.get("https://t.me/+Tr51Cs7pcNdjNDUx", "") 
 
 DB_NAME = os.environ.get("DB_NAME","")     
 
@@ -18,6 +18,6 @@ FLOOD = int(os.environ.get("FLOOD", "10"))
 
 START_PIC = os.environ.get("START_PIC", "")
 
-ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '').split()]
+ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('5347606696', '').split()]
 
 PORT = os.environ.get("PORT", "8080")
